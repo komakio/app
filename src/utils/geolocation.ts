3558b031a3b630e2dc62bd6a31d8c582fb7b5054
Theo Mathieu <tmathieu.github@fastmail.com>
2020-03-22 07:34:37 +0100

    feat: errors geoloc

---
+++ src/utils/geolocation.ts
@@ -0,0 +1,25 @@
+import { GeolocationResponse } from '@react-native-community/geolocation';
+import RNGeolocation from '@react-native-community/geolocation';
+
+export class Geolocation {
+  public static get(): Promise<GeolocationResponse> {
+    return new Promise(async (resolve, reject) => {
+      RNGeolocation.getCurrentPosition(
+        info => resolve(info),
+        error => {
+          if (error.code === error.PERMISSION_DENIED) {
+            return reject('GEOLOCATION_PERMISSION_DENIED');
+          }
+          if (error.code === error.POSITION_UNAVAILABLE) {
+            return reject('GEOLOCATION_POSITION_UNAVAILABLE');
+          }
+          if (error.code === error.TIMEOUT) {
+            return reject('GEOLOCATION_TIMEOUT');
+          }
+          reject(error);
+        },
+        { enableHighAccuracy: true }
+      );
+    });
+  }
+}
