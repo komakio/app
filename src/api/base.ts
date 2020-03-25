c690343127f56e53a60fa566c1330efa9181d016
Theo Mathieu <tmathieu.github@fastmail.com>
2020-03-25 16:35:54 +0100

    feat: apple login

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,4 +1,4 @@
 import Axios from 'axios';
 
-const baseUrl = 'http://192.168.0.122:3000';
+const baseUrl = 'http://192.168.0.122:3100';
 export const axiosInstance = Axios.create({ baseURL: baseUrl });
