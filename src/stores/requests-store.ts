f4769683060a6ae62dc003cc16baebcc9a9d8ec5
Theo Mathieu <tmathieu.github@fastmail.com>
2020-03-26 16:48:41 +0100

    feat: get requests

--- src/stores/requests-store.ts
+++ src/stores/requests-store.ts
@@ -1,10 +1,27 @@
 import { RootStore } from './root-store';
 import { RequestsApi } from '../api/request';
+import { autorun } from 'mobx';
 
 export class RequestsStore {
   private userStore = this.rootStore.userStore;
 
-  constructor(private rootStore: RootStore) {}
+  constructor(private rootStore: RootStore) {
+    autorun(() => {
+      if (this.userStore.profile) {
+        this.getRequests();
+      }
+    })
+  }
+
+  public async getRequests() {
+    this.userStore.waitReady();
+    if (!this.userStore.profile?._id) {
+      return;
+    }
+    
+    const requests = await RequestsApi.getAllRequests(this.userStore.accessToken.token, this.userStore.profile._id);
+    console.log(requests)
+  }
 
   public async createRequest(): Promise<void> {
     if (!this.rootStore.userStore.profile) {
