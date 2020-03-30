01fdabd8626d5d88c80deb68ea96f0614b84d2f7
Theo Mathieu <tmathieu.github@fastmail.com>
2020-03-30 19:37:44 +0200

    fix: initial registration token

--- src/stores/notifications-store.ts
+++ src/stores/notifications-store.ts
@@ -1,20 +1,29 @@
 import { RootStore } from './root-store';
 import { Messaging } from '../utils/messaging';
 import { UsersApi } from '../api/user';
+import { autorun, observable } from 'mobx';
 
 export class NotificationsStore {
+  @observable
   private registrationToken: string;
 
-  constructor(private rootStore: RootStore) {}
+  constructor(private rootStore: RootStore) {
+    autorun(() => {
+      if (
+        this.rootStore.userStore?.accessToken?.token &&
+        this.registrationToken
+      ) {
+        this.synchronizeToken();
+      }
+    });
+  }
 
   public async registerForNotifications() {
     await Messaging.requestPermission();
     await Messaging.registerForRemoteNotifications();
     Messaging.onMessage();
     Messaging.onToken((registrationToken) => {
-      console.log(registrationToken);
       this.registrationToken = registrationToken;
-      this.synchronizeToken();
     });
   }
 
