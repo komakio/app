591899f59ecf6917a8e6c4c8b7420aeaf17683b5
Théo Mathieu <tmathieu.github@fastmail.com>
2020-04-03 15:45:58 +0200

    feat: sentry error reporting (#112)

---
+++ src/stores/exceptions-store.ts
@@ -0,0 +1,23 @@
+import * as Sentry from '@sentry/react-native';
+
+import { Environment } from '../environment';
+import { RootStore } from './root-store';
+
+export class ExceptionsStore {
+  public rootStore: RootStore;
+
+  constructor(rootStore: RootStore) {
+    this.rootStore = rootStore;
+
+    if (Environment.environment === 'development') {
+      return;
+    }
+    try {
+      Sentry.init({
+        dsn: 'https://70da0c24b7a04d41939443ccc906ef98@sentry.anchor.io/16',
+      });
+    } catch {
+      /* Do nothing - TODO remove that when production deployment */
+    }
+  }
+}
