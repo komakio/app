62f06e9e50533ecf9ede0a39c8e582d8329649c2
Theo Mathieu <tmathieu.github@fastmail.com>
2020-04-11 18:18:03 +0200

    fix: receiving a push notification reloads the requests list

--- src/utils/messaging.ts
+++ src/utils/messaging.ts
@@ -38,20 +38,13 @@ export class Messaging {
 
   public static onMessage(cb?: () => void) {
     messaging().onMessage(async (remoteMessage) => {
-      console.log('FCM Message Data:', remoteMessage.data);
-      // Alert.alert('INAPP');
-      // Alert.alert('MESSAGE');
-
-      //   // Update a users messages list using AsyncStorage
-      //   const currentMessages = await AsyncStorage.getItem('messages');
-      //   const messageArray = JSON.parse(currentMessages);
-      //   messageArray.push(remoteMessage.data);
-      //   await AsyncStorage.setItem('messages', JSON.stringify(messageArray));
+      cb();
+      console.log('Foreground', remoteMessage);
     });
 
     messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-      console.log('Message handled in the background!', remoteMessage);
-      // Alert.alert('BACKGROUND');
+      console.log('Background', remoteMessage);
+      cb();
     });
 
     messaging().onNotificationOpenedApp((message) => {
