2e609d310bac54ffd8be54007ad759739695ecd6
Theo Mathieu <tma@ocean.io>
2020-04-17 07:34:13 +0200

    feat: hermes support

--- src/app.tsx
+++ src/app.tsx
@@ -4,14 +4,6 @@ import { Router } from './router';
 import { Layout } from './layout/layout';
 
 import { includeCodePush } from './code-push';
-import { Alert } from 'react-native';
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-ignore
-// @ts-ignore
-const isHermes = () => !!global.HermesInternal;
-if (isHermes()) {
-  Alert.alert('Hermes');
-}
 
 export const App = includeCodePush(() => {
   return (
