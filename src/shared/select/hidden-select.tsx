60d8cbf7984416fa129fdb40ce4a21a879ee85c7
Theo Mathieu <tma@ocean.io>
2020-04-19 15:17:27 +0200

    feat: change language on android

--- src/shared/select/hidden-select.tsx
+++ src/shared/select/hidden-select.tsx
@@ -1,32 +1,39 @@
 import React, { useRef, memo, FC, useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { observer } from 'mobx-react-lite';
+import { StyleSheet, Platform, View, Picker as RNPicker } from 'react-native';
 import { useTranslation } from 'react-i18next';
-import { languages } from '@i18n/index';
 import PickerSelect from 'react-native-picker-select';
-import { useLanguageStore } from '@stores';
 
 const styles = StyleSheet.create({
   input: {
     display: 'none',
+  },
+  androidHiddenPicker: {
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    height: 70,
+    opacity: 0,
   },
 });
 
 interface HiddenSelectProps {
   open: boolean;
   onClose: () => void;
+  onChoose: (value: string) => void;
   items: PickerSelect['props']['items'];
   initialValue: string;
 }
 
 export const HiddenSelect: FC<HiddenSelectProps> = memo(
-  ({ open, onClose, initialValue, items }) => {
+  ({ open, onClose, initialValue, items, children, onChoose }) => {
     const pickerSelectRef = useRef<PickerSelect>();
-    const languageStore = useLanguageStore();
     const { t } = useTranslation();
     const [value, setValue] = useState<string>(initialValue);
 
     useEffect(() => {
+      if (Platform.OS === 'android') {
+        return;
+      }
       if (open && pickerSelectRef) {
         const state: any = pickerSelectRef.current.state;
         state.showPicker = true;
@@ -35,22 +42,44 @@ export const HiddenSelect: FC<HiddenSelectProps> = memo(
       }
     }, [open]);
 
+    if (Platform.OS === 'android') {
+      return (
+        <View>
+          {children}
+          <RNPicker
+            style={styles.androidHiddenPicker}
+            selectedValue={value}
+            onValueChange={onChoose}
+          >
+            {items.map((item) => (
+              <RNPicker.Item
+                key={item.value}
+                label={item.label}
+                value={item.value}
+              />
+            ))}
+          </RNPicker>
+        </View>
+      );
+    }
+
     return (
-      <PickerSelect
-        ref={pickerSelectRef}
-        value={value}
-        style={{
-          inputIOS: styles.input,
-          //   inputAndroid: styles.input,
-        }}
-        // useNativeAndroidPickerStyle={false}
-        placeholder={{}}
-        doneText={t('ACTIONS_DONE')}
-        onDonePress={() => languageStore.setLanguage(value)}
-        onClose={onClose}
-        onValueChange={setValue}
-        items={items.map((i) => ({ ...i, key: i.value }))}
-      />
+      <>
+        {children}
+        <PickerSelect
+          ref={pickerSelectRef}
+          value={value}
+          style={{
+            inputIOS: styles.input,
+          }}
+          placeholder={{}}
+          doneText={t('ACTIONS_DONE')}
+          onDonePress={() => onChoose(value)}
+          onClose={onClose}
+          onValueChange={setValue}
+          items={items.map((i) => ({ ...i, key: i.value }))}
+        />
+      </>
     );
   }
 );
