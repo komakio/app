5c6cb84bbfd9be84781993f7d165e153b96dcc4d
Kristina Arezina <kristinaarezina@Kristinas-MacBook-Pro.local>
2020-04-29 08:43:34 -0400

    added margin bottom to back button

--- src/modules/nav-bar/nav-bar.tsx
+++ src/modules/nav-bar/nav-bar.tsx
@@ -17,6 +17,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
     height: bottomNavbarHeight,
+    marginBottom: 15,
   },
   transparent: {
     backgroundColor: 'transparent',
